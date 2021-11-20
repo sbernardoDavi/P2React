8820eb34712354f4f1519d2650a01412bd26e2a6
Davi <sbernardo.davi@outlook.com>
2021-11-20 15:22:25 -0300

    fix: finalizando o Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -12,17 +12,32 @@ import { Nav } from 'react-bootstrap';
 
 function Header(){
     return(
-        <header class={styles.header}>
-            <a class={styles.logo} href="#"><img src={logoPrincipal} width="auto" height="30px" ></img></a>
-            <Nav class ={styles.navbar} >
-                <ul class={styles.list}>
-                    <div> <li class ={styles.item}> <a class= "" href=""><img src={lupa} width="auto" height="20px"></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href=""><img src={usuario} width="auto" height="20px"></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href="#"><img src={Line} ></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href="#"><img src={carrinho} width="auto" height="20px"></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href="#"><img src={brasil} width="auto" height="20px"></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href="#"><img src={espanha} width="auto" height="20px"></img></a> </li></div>
-                    <div> <li class ={styles.item}> <a class= "" href="#"><img src={gra} width="auto" height="20px"></img></a> </li></div>
+        <header className={styles.header}>
+            <a className={styles.logo} href="#"><img src={logoPrincipal} width="auto" height="30px" ></img></a>
+                <div className={styles.busca}>
+                  <div className={styles.textobusca}>
+                    <p>O que você está procurando?</p>
+                  </div>
+                  <a className={styles.imglupa} href="#">
+                    <img src={lupa} width="auto" height="20px"></img>
+                  </a>
+                </div>
+            <Nav class ="">
+                <ul className={styles.list}>
+                    <div className={styles.minhaconta}> 
+                        <div>
+                            <li className={styles.item}> <a class= "" href=""><img src={usuario} width="auto" height="20px"></img></a> </li>
+                        </div>
+                        <div>
+                            <p className={styles.textominhaconta}> Minha conta</p>
+
+                        </div>
+                    </div>
+                    <div className={styles.carrinho}> <li class ={styles.item}> <a class= "" href="#"><img src={carrinho} width="auto" height="20px"></img></a> </li></div>
+                    <div> <li className={styles.item}> <a class= "" href="#"><img src={Line} ></img></a> </li></div>
+                    <div clasNames={styles.brasil}> <li class ={styles.item}> <a class= "" href="#"><img src={brasil} width="auto" height="20px"></img></a> </li></div>
+                    <div> <li className ={styles.item}> <a class= "" href="#"><img src={espanha} width="auto" height="20px"></img></a> </li></div>
+                    <div className={styles.gra}> <li class ={styles.item}> <a class= "" href="#"><img src={gra} width="auto" height="20px"></img></a> </li></div>
                 </ul>
             </Nav>
           
